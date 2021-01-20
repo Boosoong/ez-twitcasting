c07a7679203e1443e216aceacea763e690ee596e
Boosoong <68192139+Boosoong@users.noreply.github.com>
2021-01-21 05:29:28 +0900

    Update ez-twitcasting.user.js

--- ez-twitcasting.user.js
+++ ez-twitcasting.user.js
@@ -6,7 +6,6 @@
 // @match        *://*.twitcasting.tv/*
 // @supportURL  https://github.com/Boosoong/ez-twitcasting/issues
 // @homepageURL https://github.com/Boosoong/ez-twitcasting/
-// @downloadURL https://github.com/Boosoong/ez-twitcasting/raw/main/ez-twitcasting.user.js
 // @grant        none
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 // ==/UserScript==
