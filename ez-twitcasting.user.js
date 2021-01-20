6bd765e1d16fa723f20d53b3e0430bdb72c8ade4
Boosoong <68192139+Boosoong@users.noreply.github.com>
2021-01-21 05:49:07 +0900

    Update ez-twitcasting.user.js

--- ez-twitcasting.user.js
+++ ez-twitcasting.user.js
@@ -3,13 +3,13 @@
 // @version      0.1
 // @description  엔터키로 코멘트를 달 수 있게 합니다. 줄바꿈은 Shift+엔터키로 가능합니다.
 // @author       bs
-// @match        *://*.twitcasting.tv/*
-// @supportURL  https://github.com/Boosoong/ez-twitcasting/issues
-// @homepageURL https://github.com/Boosoong/ez-twitcasting/
+// @include      *://twitcasting.tv/*
+// @supportURL   https://github.com/Boosoong/ez-twitcasting/issues
+// @homepageURL  https://github.com/Boosoong/ez-twitcasting/
 // @downloadURL https://github.com/Boosoong/ez-twitcasting/raw/main/ez-twitcasting.user.js
 // @updateURL   https://github.com/Boosoong/ez-twitcasting/raw/main/ez-twitcasting.user.js
 // @grant        none
-// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
 // ==/UserScript==
 
 (function() {
