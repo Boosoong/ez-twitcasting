9871415d05026ffe0e7534ab4259cfd59735b618
Boosoong <68192139+Boosoong@users.noreply.github.com>
2021-01-21 04:55:09 +0900

    Add files via upload

---
+++ ez-twitcasting.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         ez twitcasting
+// @version      0.1
+// @description  엔터키로 코멘트를 달 수 있게 합니다. 줄바꿈은 Shift+엔터키로 가능합니다.
+// @author       bs
+// @match        *://*.twitcasting.tv/*
+// @grant        none
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js
+// ==/UserScript==
+ 
+(function() {
+    'use strict';
+ 
+$(document).on('keypress','textarea', function(e){
+        if(e.keyCode == 13 && !e.shiftKey) {
+            $(this).blur();
+            $('.tw-button-primary').click();
+            return false;
+        } else if(e.shiftKey && e.keyCode == 13) {
+            return true;
+ 
+        }
+    });
+})();
